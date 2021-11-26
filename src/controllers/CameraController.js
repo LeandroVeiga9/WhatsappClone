739d8cd5e432f8b38ac83fbf28a172ee4d7b7c9f
LeandroVeiga9 <94861857+LeandroVeiga9@users.noreply.github.com>
2021-11-26 17:45:17 -0300

    Add files via upload

---
+++ src/controllers/CameraController.js
@@ -0,0 +1,22 @@
+export class CameraController{
+
+    constructor(videoEl){
+
+        this._videoEl = videoEl
+
+        navigator.mediaDevices.getUserMedia({
+
+            video: true
+
+        }).then(stream=>{
+
+            this._videoEl.srcObject = new MediaStream(stream);
+            this._videoEl.play()
+
+        }).catch(err=>{
+            console.error(err);
+        })
+
+    }
+
+}
