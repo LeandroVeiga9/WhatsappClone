fa3a00be57fe808208abcf4c22f4ed09ccb65ba5
LeandroVeiga9 <94861857+LeandroVeiga9@users.noreply.github.com>
2021-11-29 17:48:46 -0300

    upload firebase

---
+++ src/utils/Firebase.js
@@ -0,0 +1,74 @@
+const firebase = require('firebase')
+require('firebase/firestore')
+
+export class Firebase {
+
+    constructor(){
+
+        this._config = {
+            apiKey: "AIzaSyCwvv_Vbi8NxuNOq7r9Fq9Iz0c-gB7jUWw",
+            authDomain: "whatsapp-clone-ee917.firebaseapp.com",
+            projectId: "whatsapp-clone-ee917",
+            storageBucket: "whatsapp-clone-ee917.appspot.com",
+            messagingSenderId: "207139810503",
+            appId: "1:207139810503:web:61680920bc557ba93962e2"
+          }
+
+        this.init()
+
+    }
+
+    init(){
+
+        if(!this._initialized){
+
+            firebase.initializeApp(this._config)
+            
+            firebase.firestore().settings({
+                timestampsInSnapshots: true
+            })
+            
+            this._initialized = true
+
+        }
+
+    }
+
+    static db(){
+
+        return firebase.firestore()
+
+    }
+
+    static hd(){
+
+        return firebase.storage()
+
+    }
+
+    initAuth(){
+
+        return new Promise((s, f) =>{
+
+            let provider = new firebase.auth.GoogleAuthProvider()
+
+            firebase.auth().signInWithPopup(provider).then(result=>{
+
+                let token = result.credential.accessToken
+                let user = result.user
+
+                s({
+                    user, token
+                })
+
+            }).catch(err=>{
+
+                f(err)
+
+            })
+
+        })
+
+    }
+
+}
